8c0308ec998c143b23b9d2cac82b19bee73a93e4
Anish <anish.alwekar@gmail.com>
2023-05-05 08:51:06 +0530

    commit-1

---
+++ src/main/java/com/app/AzureAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureAppServiceApplication.class, args);
+	}
+
+}
